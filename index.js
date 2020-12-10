e1bb87d1f3a9630171137aee047fd6e34b8d42a3
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-10 22:17:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Your code here
 function mapToNegativize(sourceArray) {
-  
+  let newArray = []
+  for (const element of sourceArray) {
+    newArray.push(-Math.abs(element))
+  }
+  newArray
 }
