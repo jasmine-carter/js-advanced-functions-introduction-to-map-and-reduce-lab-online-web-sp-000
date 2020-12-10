cb28560fb76610d52e4d5b0fe92a7c3fb134fd8e
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-10 22:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 function mapToNegativize(sourceArray) {
   let newArray = []
   for (const element of sourceArray) {
+    if (Math.sign(element) === 1) {
     newArray.push(-Math.abs(element))
+    }
+    else
+    newArray.push(Math.abs(element))
   }
   return newArray
 }
